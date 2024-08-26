927d054fbc97b067ab6e6c141c8be9c69ed5eed3
opilane <opilane@HP-CZC2349HTY>
2024-08-26 16:22:08 +0300

    Add project files.

---
+++ TsarevTARpv23_CSharp/Funktsioonid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsarevTARpv23_CSharp
+{
+    public class Funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}", nimi);
+        }
+
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+        // Loo Arvuta() funktsioon, mis sõltub 3 parameetrist:tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile
+        public static string Arvuta(int arv1, int arv2, char tehe)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            double j = Convert.ToDouble(table.Compute(arv1.ToString() + tehe.ToString() + arv2.ToString(), String.Empty));
+
+            j = Math.Round(j, 0);
+            string vastus = "";
+            string nur = "nurr ";
+            for (int i = 0; i < Math.Abs(Convert.ToInt32(j)); i++)
+            {
+                vastus = vastus + nur;
+            }
+            return vastus;
+        }
+    }
+
+}
---
+++ TsarevTARpv23_CSharp/MainClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsarevTARpv23_CSharp
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //I.osa Andmetüübid Alamfunktsioonid, If
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello, {0}",nimetus);
+            Funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimene arv: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus = Funktsioonid.Liitmine(a, b);
+            Console.WriteLine(vastus);
+            double arv = 5.123456;
+            vastus=Funktsioonid.Liitmine(a, (int)arv);
+            Console.WriteLine(vastus);
+            char taht = 'A';
+            if (vastus == 0)
+            {
+                Console.WriteLine(taht);
+            }
+            else
+            {
+                Console.WriteLine(vastus);
+            }
+            Console.WriteLine("Arv1: ");
+            int arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arv2: ");
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tehe: ");
+            char tehe = char.Parse(Console.ReadLine());
+            if (tehe == '+')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 + arv2);
+            }
+            else if (tehe == '-')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 - arv2);
+            }
+            else if (tehe == '*')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 * arv2);
+            }
+            else if (tehe == '/')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 / arv2);
+            }
+            Console.WriteLine("Tere tulemast!\nMis on sinu nimi?");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere " + eesnimi);
+            //&&-and, ||-or, !-not 
+            if (eesnimi.ToUpper() == "JUKU")
+            {
+                Console.WriteLine("Lähme kinno!\nKui vana sa oled?");
+                int vanus = int.Parse(Console.ReadLine());
+                if (vanus < 0 || vanus > 100)
+                {
+                    Console.WriteLine("Viga!");
+                }
+                else if (vanus <= 6)
+                {
+                    Console.WriteLine("Tasuta pilet!");
+                }
+                else if (vanus <= 15)
+                {
+                    Console.WriteLine("Lastepilet!");
+                }
+                else if (vanus <= 65)
+                {
+                    Console.WriteLine("Täispilet!");
+                }
+                else if (vanus <= 100)
+                {
+                    Console.WriteLine("Sooduspilet!");
+                }
+            }
+        }
+    }
+}
